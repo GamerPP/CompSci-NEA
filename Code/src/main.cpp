e5c45a2a94892a268cc6f64fba1111f326afbe76
GamePP <callumtomkins260505@gmail.com>
2022-06-28 12:04:33 +0100

    include raylib graohis library and make template

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <raylib.h>
 
-int main(int argc, char** argv) {
-    std::cout << "Hello, World!\n";
+#include "Math.hpp"
+
+Math::Vec2<int> windowSize;
+int main() {
+    while (true) {
+        std::cout << "Welcome to chess!\n";
+
+
+        while (true) {
+            if (WindowShouldClose()) goto endGame;
+            BeginDrawing();
+                ClearBackground(BLACK);
+            EndDrawing();
+        }
+endGame:
+        CloseWindow();
+    }
+
+exit:
+    CloseWindow();
+    return 0;
 }
