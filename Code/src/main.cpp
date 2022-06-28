4e7b5d000934532a8f7ad7139cfcc1dfebc6265f
GamePP <callumtomkins260505@gmail.com>
2022-06-28 09:46:12 +0100

    include SDL2 as submodule

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+struct example {
+    int a = 10;
+    int b = 20;
+    int c = 20;
+};
+
 int main(int argc, char** argv) {
-    std::cout << "Hello world!\n";
+    example ex = {10, 20, 30};
+    int* p = &ex.a;
+    std::cout << p << ':' << *p << std::endl;
+    p += 1;
+    std::cout << p << ':' << *p << std::endl;
+    p += 1;
+    std::cout << p << ':' << *p << std::endl;
 }
