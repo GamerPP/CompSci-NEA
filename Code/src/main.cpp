fc85dff0d931da5925eb94a40633e873bfd1fe7f
GamePP <callumtomkins260505@gmail.com>
2022-06-28 11:23:56 +0100

    delete everything start again new idea

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,17 +1,5 @@
 #include <iostream>
 
-struct example {
-    int a = 10;
-    int b = 20;
-    int c = 20;
-};
-
 int main(int argc, char** argv) {
-    example ex = {10, 20, 30};
-    int* p = &ex.a;
-    std::cout << p << ':' << *p << std::endl;
-    p += 1;
-    std::cout << p << ':' << *p << std::endl;
-    p += 1;
-    std::cout << p << ':' << *p << std::endl;
+    std::cout << "Hello, World!\n";
 }
